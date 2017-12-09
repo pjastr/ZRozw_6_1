01363f70e551f7439cdf03fee6379397d9795f3b
Piotrek <@>
2017-12-09 16:41:33 +0100

    przeslanie plików

---
+++ ZRozw_6_1/Osoba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRozw_6_1
+{
+    class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+
+        public Osoba() { }
+
+        public Osoba(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+
+        public virtual void WypiszInfo()
+        {
+            Console.WriteLine("Imię i nazwisko: {0} {1}", imie, nazwisko);
+        }
+    }
+}
---
+++ ZRozw_6_1/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRozw_6_1
+{
+    class Student:Osoba
+    {
+        private int rok;
+        private int numerGrupy;
+        private int numerAlbumu;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string imie, string nazwisko, int rok, int numerGrupy, int numerAlbumu)
+            :base(imie, nazwisko)
+        {
+            this.rok = rok;
+            this.numerGrupy = numerGrupy;
+            this.numerAlbumu = numerAlbumu;
+        }
+
+        public override void WypiszInfo()
+        {
+            base.WypiszInfo();
+            Console.WriteLine("Rok: {0}",rok);
+            Console.WriteLine("Numer grupy: {0}", numerGrupy);
+            Console.WriteLine("Numer albumu: {0}", numerAlbumu);
+        }
+    }
+}
